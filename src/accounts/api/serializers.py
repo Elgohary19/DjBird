ea3eb55a484aa162677d0e675e89c830d0feddac
elgohary19 <mouse.fcis@gmail.com>
2020-02-06 11:42:04 +0200

    fix image2

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from rest_framework import serializers
+from ..models import UserProfile
 
 
 User = get_user_model()
@@ -10,13 +11,14 @@ class UserDisplaySerializer(serializers.ModelSerializer):
     follower_count = serializers.SerializerMethodField()
     url = serializers.SerializerMethodField()
     class Meta:
-        model = User
+        model = UserProfile
         fields = (
             'username',
             'first_name',
             'last_name',
             'follower_count',
             'url',
+            'get_avatar',
             #'email',
         )
 
